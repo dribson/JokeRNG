fe246f35900ae3b480061d80914f9b8e0a90b6e6
Matthew Hein <mattheinit@gmail.com>
2025-04-17 16:17:48 -0500

    UI tweaks & skip fix
    Layout of main tab has been updated
    Added a rules button with some rules in it
    Should now no longer tell to skip on a boss blind but haven't tested this yet lol

--- JokeRNG.py
+++ JokeRNG.py
@@ -12,7 +12,7 @@ weightSkipBlind = 10
 weightRerollBoss = 10
 weightBuyUse = 50
 weightBuyTwoFromPack = 50
-weightSellJoker = 15
+weightSellJoker = 4
 weightUseSellConsumable = 40
 weightSellConsumable = 20
 weightDiscard = 35
@@ -288,14 +288,14 @@ class JokeR:
 
         self.playbtn = StringVar()
         self.playbtn.set("Start New Game")
-        ttk.Label(mainframe, text="Current Action: ").grid(column=1, row=1, sticky=E)
-        ttk.Label(mainframe, textvariable=self.step).grid(column=2, row=1, sticky=(W,E))
-        ttk.Button(mainframe, textvariable=self.playbtn, command=self.NextGameState).grid(column=3, row=1, sticky=(W, E))
-        ttk.Button(mainframe, text="Calculate Next Action (Blind Won)", command=self.NextGameStateWin).grid(column=4, row=1, sticky=(W, E))
-        ttk.Button(mainframe, text="Press to Lose", command=self.Exit).grid(column=5, row=1, sticky=W)
-
-        ttk.Label(mainframe, text="Run Variables").grid(column=1, row=2, sticky=E)
-        ttk.Label(mainframe, text="Update these before calculating next action").grid(column=2, row=2, sticky=E)
+        ttk.Button(mainframe, text="Calculate Next Action (Blind Won)", command=self.NextGameStateWin).grid(column=1, row=1, sticky=E)
+        ttk.Button(mainframe, textvariable=self.playbtn, command=self.NextGameState).grid(column=2, row=1, sticky=(W, E))
+        ttk.Label(mainframe, text="Current Action: ").grid(column=3, row=1, sticky=(W, E))
+        ttk.Label(mainframe, textvariable=self.step).grid(column=4, row=1, sticky=W)
+        ttk.Button(mainframe, text="Press to Lose", command=self.Exit).grid(column=1, row=2, sticky=E)
+        ttk.Button(mainframe, text="Rules", command=self.Rules).grid(column=2, row=2, sticky=W)
+        ttk.Label(mainframe, text="Run Variables").grid(column=1, row=3, sticky=E)
+        ttk.Label(mainframe, text="Update Before Calculation").grid(column=2, row=3, sticky=W)
 
         # Calculation Variables
         self.handSize = IntVar()
@@ -331,41 +331,41 @@ class JokeR:
         self.shopRerolls = IntVar()
         self.shopRerolls.set(3)
 
-        ttk.Label(mainframe, text="Hand Size: ").grid(column=1, row=3, sticky=E)
-        ttk.Label(mainframe, textvariable=(self.handSize)).grid(column=2, row=3, sticky=(W, E))
-        ttk.Button(mainframe, text="+", command=self.AddHandSize).grid(column=3, row=3, sticky=(W, E))
-        ttk.Button(mainframe, text="-", command=self.SubHandSize).grid(column=4, row=3, sticky=W)
+        ttk.Label(mainframe, text="Hand Size: ").grid(column=1, row=4, sticky=E)
+        ttk.Label(mainframe, textvariable=(self.handSize)).grid(column=2, row=4, sticky=(W, E))
+        ttk.Button(mainframe, text="+", command=self.AddHandSize).grid(column=3, row=4, sticky=(W, E))
+        ttk.Button(mainframe, text="-", command=self.SubHandSize).grid(column=4, row=4, sticky=W)
 
-        ttk.Label(mainframe, text="Joker Slots: ").grid(column=1, row=4, sticky=E)
-        ttk.Label(mainframe, textvariable=(self.jokerSize)).grid(column=2, row=4, sticky=(W, E))
-        ttk.Button(mainframe, text="+", command=self.AddJokerSize).grid(column=3, row=4, sticky=(W, E))
-        ttk.Button(mainframe, text="-", command=self.SubJokerSize).grid(column=4, row=4, sticky=W)
+        ttk.Label(mainframe, text="Joker Slots: ").grid(column=1, row=5, sticky=E)
+        ttk.Label(mainframe, textvariable=(self.jokerSize)).grid(column=2, row=5, sticky=(W, E))
+        ttk.Button(mainframe, text="+", command=self.AddJokerSize).grid(column=3, row=5, sticky=(W, E))
+        ttk.Button(mainframe, text="-", command=self.SubJokerSize).grid(column=4, row=5, sticky=W)
 
-        ttk.Label(mainframe, text="Consumable Slots: ").grid(column=1, row=5, sticky=E)
-        ttk.Label(mainframe, textvariable=(self.consumableSize)).grid(column=2, row=5, sticky=(W, E))
-        ttk.Button(mainframe, text="+", command=self.AddConsumeSize).grid(column=3, row=5, sticky=(W, E))
-        ttk.Button(mainframe, text="-", command=self.SubConsumeSize).grid(column=4, row=5, sticky=W)
+        ttk.Label(mainframe, text="Consumable Slots: ").grid(column=1, row=6, sticky=E)
+        ttk.Label(mainframe, textvariable=(self.consumableSize)).grid(column=2, row=6, sticky=(W, E))
+        ttk.Button(mainframe, text="+", command=self.AddConsumeSize).grid(column=3, row=6, sticky=(W, E))
+        ttk.Button(mainframe, text="-", command=self.SubConsumeSize).grid(column=4, row=6, sticky=W)
 
-        ttk.Label(mainframe, text="Shop Slots: ").grid(column=1, row=6, sticky=E)
-        ttk.Label(mainframe, textvariable=(self.shopSize)).grid(column=2, row=6, sticky=(W, E))
-        ttk.Button(mainframe, text="+", command=self.AddShopSize).grid(column=3, row=6, sticky=(W, E))
-        ttk.Button(mainframe, text="-", command=self.SubShopSize).grid(column=4, row=6, sticky=W)
+        ttk.Label(mainframe, text="Shop Slots: ").grid(column=1, row=7, sticky=E)
+        ttk.Label(mainframe, textvariable=(self.shopSize)).grid(column=2, row=7, sticky=(W, E))
+        ttk.Button(mainframe, text="+", command=self.AddShopSize).grid(column=3, row=7, sticky=(W, E))
+        ttk.Button(mainframe, text="-", command=self.SubShopSize).grid(column=4, row=7, sticky=W)
 
-        ttk.Label(mainframe, text="Owned Jokers: ").grid(column=1, row=7, sticky=E)
-        ttk.Label(mainframe, textvariable=(self.ownedJokers)).grid(column=2, row=7, sticky=(W, E))
-        ttk.Button(mainframe, text="+", command=self.AddOwnedJoker).grid(column=3, row=7, sticky=(W, E))
-        ttk.Button(mainframe, text="-", command=self.SubOwnedJoker).grid(column=4, row=7, sticky=W)
+        ttk.Label(mainframe, text="Owned Jokers: ").grid(column=1, row=8, sticky=E)
+        ttk.Label(mainframe, textvariable=(self.ownedJokers)).grid(column=2, row=8, sticky=(W, E))
+        ttk.Button(mainframe, text="+", command=self.AddOwnedJoker).grid(column=3, row=8, sticky=(W, E))
+        ttk.Button(mainframe, text="-", command=self.SubOwnedJoker).grid(column=4, row=8, sticky=W)
 
-        ttk.Label(mainframe, text="Owned Consumables: ").grid(column=1, row=8, sticky=E)
-        ttk.Label(mainframe, textvariable=(self.ownedConsumables)).grid(column=2, row=8, sticky=(W, E))
-        ttk.Button(mainframe, text="+", command=self.AddOwnedConsume).grid(column=3, row=8, sticky=(W, E))
-        ttk.Button(mainframe, text="-", command=self.SubOwnedConsume).grid(column=4, row=8, sticky=W)
+        ttk.Label(mainframe, text="Owned Consumables: ").grid(column=1, row=9, sticky=E)
+        ttk.Label(mainframe, textvariable=(self.ownedConsumables)).grid(column=2, row=9, sticky=(W, E))
+        ttk.Button(mainframe, text="+", command=self.AddOwnedConsume).grid(column=3, row=9, sticky=(W, E))
+        ttk.Button(mainframe, text="-", command=self.SubOwnedConsume).grid(column=4, row=9, sticky=W)
 
 
-        ttk.Label(mainframe, text="Manually Select Options:").grid(column=1, row=9, sticky=E)
-        ttk.Label(mainframe, text="# Items to Pick").grid(column=2, row=9, sticky=(W, E))
-        ttk.Label(mainframe, text="# Total Items Available").grid(column=3, row=9, sticky=(W, E))
-        ttk.Label(mainframe, text="Output").grid(column=4, row=9, sticky=W)
+        ttk.Label(mainframe, text="Manually Select Options:").grid(column=1, row=10, sticky=E)
+        ttk.Label(mainframe, text="# Items to Pick").grid(column=2, row=10, sticky=(W, E))
+        ttk.Label(mainframe, text="# Total Items Available").grid(column=3, row=10, sticky=(W, E))
+        ttk.Label(mainframe, text="Output").grid(column=4, row=10, sticky=W)
 
         self.packText = StringVar()
         self.packText.set("")
@@ -373,10 +373,10 @@ class JokeR:
         self.packInput.set("")
         self.packMax = StringVar()
         self.packMax.set("")
-        ttk.Button(mainframe, text="Select Cards", command=self.GenerateNumbersOfPackCards).grid(column=1, row=10, sticky=E)
-        ttk.Entry(mainframe, width=2, textvariable=self.packInput).grid(column=2, row=10, sticky=(W, E))
-        ttk.Entry(mainframe, width=2, textvariable=self.packMax).grid(column=3, row=10, sticky=(W, E))
-        ttk.Label(mainframe, textvariable=(self.packText)).grid(column=4, row=10, sticky=W)
+        ttk.Button(mainframe, text="Select Cards", command=self.GenerateNumbersOfPackCards).grid(column=1, row=11, sticky=E)
+        ttk.Entry(mainframe, width=2, textvariable=self.packInput).grid(column=2, row=11, sticky=(W, E))
+        ttk.Entry(mainframe, width=2, textvariable=self.packMax).grid(column=3, row=11, sticky=(W, E))
+        ttk.Label(mainframe, textvariable=(self.packText)).grid(column=4, row=11, sticky=W)
 
         for child in mainframe.winfo_children(): 
             child.grid_configure(padx=5, pady=5)
@@ -395,6 +395,7 @@ class JokeR:
             # Generate which deck to use
             # Set defaults for variables based on deck chosen
             randomDeck = random.randint(0, 14)
+            randomDeck = 13 # TODO testing just with plasma deck
             if(randomDeck == 0):
                 self.step.set(f"Action {self.currentAction.get()}: Select the Red Deck")
             elif(randomDeck == 1):
@@ -443,7 +444,7 @@ class JokeR:
             # Pick up to 5 cards (highly prioritise 5 cards)
             # Display that action
 
-            if random.randint(1, 100) <= weightSellJoker and self.ownedJokers.get() > 0: # Randomly sell a Joker, if one is owned
+            if random.randint(1, 100) <= (weightSellJoker * self.ownedJokers.get()) and self.ownedJokers.get() > 0: # Randomly sell a Joker, if one is owned
                 randJoker = random.randint(1, self.ownedJokers.get())
                 self.step.set(f"Action {self.currentAction.get()}: Sell the Joker currently in Slot #{randJoker}")
                 self.ownedJokers.set(self.ownedJokers.get() - 1)
@@ -489,7 +490,6 @@ class JokeR:
             else:
                 self.step.set(f"Action {self.currentAction.get()}: Select the next Blind")
                 self.gameState.set(1)
-            pass
 
         else: # unhandled
             print("Unhandled game state. Resetting to new game state")
@@ -520,7 +520,7 @@ class JokeR:
                 self.actionsShop.set(self.actionsShop.get() + 1)
                 self.step.set(f"Action {self.currentAction.get()}: Reroll the Shop")
 
-            elif randShop == 4 and self.ownedJokers.get() > 0 and random.randint(1, 100) <= weightSellJoker:
+            elif randShop == 4 and self.ownedJokers.get() > 0 and random.randint(1, 100) <= (weightSellJoker * self.ownedJokers.get()):
                 # Sell a Random Joker
                 randJoker = random.randint(1, self.ownedJokers.get())
                 self.step.set(f"Action {self.currentAction.get()}: Sell the Joker currently in Slot #{randJoker}")
@@ -565,13 +565,50 @@ class JokeR:
         
     def NextGameStateWin(self, *args):
         if(self.gameState.get() == 1):
-            self.currentBlind.set((self.currentBlind.get() + 1) % 3)
+            self.currentBlind.set(self.currentBlind.get() + 1)
+            self.currentBlind.set(self.currentBlind.get() % 3)
             if(self.currentBlind.get() == 0): # If entering into the Small Blind shop, reset voucher purchased
                 self.boughtVoucher.set(True)
             self.gameState.set(2)
             self.NextGameState()
         else:
             pass
+
+    def GenerateNumbersOfPackCards(self, *args):
+        num = int(self.packInput.get())
+        max = int(self.packMax.get())
+        lst = list(range(1, max+1))
+        random.shuffle(lst)
+        nums = lst[0:num]
+        nums.sort()
+        self.packText.set(nums)
+
+    def Rules(self, *args):
+        darulez = Toplevel()
+        darulez.title = "JokeRNG Rules"
+        rulesmainframe = ttk.Frame(darulez, padding="3 3 12 12")
+        rulesmainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        darulez.columnconfigure(0, weight=1)
+        darulez.rowconfigure(0, weight=1)
+        
+        ttk.Label(rulesmainframe, text="Here are the essential rules of playing JokeRNG").grid(column=1, row=1, sticky=W)
+        ttk.Label(rulesmainframe, text="").grid(column=1, row=2, sticky=W)
+        ttk.Label(rulesmainframe, text="1. Actions provided MUST be completed as stated").grid(column=1, row=3, sticky=W)
+        ttk.Label(rulesmainframe, text="    If the action stated is impossible, skip the action").grid(column=1, row=4, sticky=W)
+        ttk.Label(rulesmainframe, text="2. An action must be completed before proceeding to the next action").grid(column=1, row=5, sticky=W)
+        ttk.Label(rulesmainframe, text="3. The player must keep assigned variables accurate").grid(column=1, row=6, sticky=W)
+        ttk.Label(rulesmainframe, text="    JokeRNG does attempt to not tell the player to take any impossible actions, but is unaware of what precisely is purchased").grid(column=1, row=7, sticky=W)
+        ttk.Label(rulesmainframe, text="        ex. JokeRNG will never tell to use an owned Consumable if no Consumables are owned").grid(column=1, row=8, sticky=W)
+        ttk.Label(rulesmainframe, text="        ex. If a Negative Joker is purchased, the player must increment the Joker Slots and Owned Jokers by 1 each").grid(column=1, row=9, sticky=W)
+        ttk.Label(rulesmainframe, text="4. If an action requires the use of selecting owned cards, use the Manual Selector at the bottom of the main window").grid(column=1, row=10, sticky=W)
+        ttk.Label(rulesmainframe, text="    The first input is how many selections must be made. The second input is the total number of possible selections").grid(column=1, row=11, sticky=W)
+        ttk.Label(rulesmainframe, text="        ex. If an Action tells to use a Death card, input 2 for selections and your current hand size for possible selections").grid(column=1, row=12, sticky=W)
+        ttk.Label(rulesmainframe, text="5. You may not perform any actions that JokeRNG does not tell you to perform").grid(column=1, row=13, sticky=W)
+        ttk.Label(rulesmainframe, text="    ex. If JokeRNG tells you to purchase a Joker, but you have no empty Joker slots, you may NOT sell a Joker. This action is considered impossible and is thus skipped").grid(column=1, row=14, sticky=W)
+        ttk.Label(rulesmainframe, text="6. If an Action tells you to skip a blind, and that skip grants one (or more) packs, you must roll the Manual Selector for the maximum possible selections").grid(column=1, row=15, sticky=W)
+        ttk.Label(rulesmainframe, text="    JokeRNG normally tells to pick either 1 or 2 options if a Mega pack is purchased, but has no way of determing if a skipped blind awared a card pack").grid(column=1, row=16, sticky=W)
+        ttk.Label(rulesmainframe, text="        ex. If a Mega Buffoon Pack skip tag is acquired, you must roll 2 out of 4 to determine which Jokers to choose").grid(column=1, row=17, sticky=W)
+        pass
 
     def Exit(self, *args):
         self.gameState.set(0)
@@ -614,15 +651,6 @@ class JokeR:
     def SubOwnedConsume(self, *args):
         self.ownedConsumables.set(self.ownedConsumables.get() - 1)
 
-    def GenerateNumbersOfPackCards(self, *args):
-        num = int(self.packInput.get())
-        max = int(self.packMax.get())
-        lst = list(range(1, max+1))
-        random.shuffle(lst)
-        nums = lst[0:num]
-        nums.sort()
-        self.packText.set(nums)
-        pass
 
 def GuiTest():
     
